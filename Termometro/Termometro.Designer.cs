a87b6d77a28dbd4a9847d105734c2ad0fedce736
Alejandro1203 <alefg2003@gmail.com>
2025-01-21 13:41:41 +0100

    Termometro

---
+++ Termometro/Termometro.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Termometro
+{
+    partial class Termometro
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.valueTemperature = new System.Windows.Forms.TrackBar();
+            this.imageTemperature = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.valueTemperature)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTemperature)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // valueTemperature
+            // 
+            this.valueTemperature.LargeChange = 1;
+            this.valueTemperature.Location = new System.Drawing.Point(3, 3);
+            this.valueTemperature.Maximum = 5;
+            this.valueTemperature.Minimum = 1;
+            this.valueTemperature.Name = "valueTemperature";
+            this.valueTemperature.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.valueTemperature.Size = new System.Drawing.Size(56, 130);
+            this.valueTemperature.TabIndex = 0;
+            this.valueTemperature.Value = 1;
+            this.valueTemperature.ValueChanged += new System.EventHandler(this.valueTemperature_ValueChanged);
+            // 
+            // imageTemperature
+            // 
+            this.imageTemperature.Image = global::Termometro.Properties.Resources.frozen;
+            this.imageTemperature.Location = new System.Drawing.Point(65, 3);
+            this.imageTemperature.Name = "imageTemperature";
+            this.imageTemperature.Size = new System.Drawing.Size(122, 130);
+            this.imageTemperature.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imageTemperature.TabIndex = 1;
+            this.imageTemperature.TabStop = false;
+            // 
+            // Termometro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.imageTemperature);
+            this.Controls.Add(this.valueTemperature);
+            this.MaximumSize = new System.Drawing.Size(194, 138);
+            this.MinimumSize = new System.Drawing.Size(194, 138);
+            this.Name = "Termometro";
+            this.Size = new System.Drawing.Size(194, 138);
+            ((System.ComponentModel.ISupportInitialize)(this.valueTemperature)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageTemperature)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar valueTemperature;
+        private System.Windows.Forms.PictureBox imageTemperature;
+    }
+}
---
+++ Termometro/Termometro.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Termometro.Properties;
+
+namespace Termometro
+{
+    public partial class Termometro: UserControl
+    {
+
+        [Browsable(true)]
+        [Category("!Creados")]
+        [Description("Invocado al poner el valor maximo de temperatura")]
+        public event EventHandler TermometroValueChanged;
+
+        public Termometro()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        public int value
+        {
+            get
+            {
+                return valueTemperature.Value;
+            }
+        }
+
+        public int maxValue
+        {
+            get{
+                return valueTemperature.Maximum;
+            }
+        }
+
+        public int cachopo
+        {
+            get
+            {
+                return valueTemperature.Value;
+            }
+
+            set 
+            {
+                if(value > 0 && value < 6)
+                {
+                    valueTemperature.Value = value;
+                }
+
+                if(value < 1)
+                {
+                    valueTemperature.Value = 1;
+                }
+
+                if (value > 5)
+                {
+                    valueTemperature.Value = 5;
+                }
+            }
+        }
+
+        private void valueTemperature_ValueChanged(object sender, EventArgs e)
+        {
+            switch (valueTemperature.Value)
+            {
+                case 1:
+                    imageTemperature.Image = Resources.frozen;
+                    break;
+
+                case 2:
+                    imageTemperature.Image = Resources.cold;
+                    break;
+
+                case 3:
+                    imageTemperature.Image = Resources.coldHot;
+                    break;
+
+                case 4:
+                    imageTemperature.Image = Resources.heat;
+                    break;
+
+                case 5:
+                    imageTemperature.Image = Resources.abrassive_heat;
+                    break;
+            }
+
+            if (this.TermometroValueChanged != null)
+            {
+
+            TermometroValueChanged(this, new EventArgs());
+            }
+        }
+    }
+}
